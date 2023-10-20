7693ae0d1fbde96d9f59010111719aa531f52152
Rodrigo <rodrigotapia90@gmail.com>
2023-10-20 18:46:16 -0300

    Added graph as dict and graph photo

--- main.py
+++ main.py
@@ -24,6 +24,54 @@ HELIOTROPE = (199, 125, 255)
 MAUVE = (224, 170, 255)
 font = pygame.font.Font(None, 36)
 
+map = {
+    'vertex1': {'vertex2'},
+    'vertex2': {'vertex3'},
+    'vertex3': {'vertex4'},
+    'vertex4': {'vertex5'},
+    'vertex5': {'vertex6'},
+    'vertex6': {'vertex7'}, #Initial point '1'
+    'vertex7': {'vertex8'},
+    'vertex8': {'vertex9', 'vertex11', 'vertex13'}, #Initial point '3'
+    'vertex9': {'vertex8', 'vertex10'},
+    'vertex10': {'vertex9', 'vertex12', 'vertex20'},
+    'vertex11': {'vertex8', 'vertex12', 'vertex18', 'vertex19'},
+    'vertex12': {'vertex10', 'vertex11'},
+    'vertex13': {'vertex8', 'vertex14', 'vertex17'},
+    'vertex14': {'vertex13', 'vertex15'},
+    'vertex15': {'vertex14', 'vertex16', 'vertex39'},
+    'vertex16': {'vertex15', 'vertex17'},
+    'vertex17': {'vertex13','vertex16', 'vertex18', 'vertex40'},
+    'vertex18': {'vertex11', 'vertex17'},
+    'vertex19': {'vertex11', 'vertex20', 'vertex32', 'vertex43'},
+    'vertex20': {'vertex10', 'vertex19', 'vertex21'},
+    'vertex21': {'vertex20', 'vertex22'},
+    'vertex22': {'vertex21', 'vertex23'},
+    'vertex23': {'vertex22', 'vertex24', 'vertex25', 'vertex26'},
+    'vertex24': {'vertex23'}, # Key vertex
+    'vertex25': {'vertex23', 'vertex29'},
+    'vertex26': {'vertex23', 'vertex27'},
+    'vertex27': {'vertex26', 'vertex28'},
+    'vertex28': {'vertex27', 'vertex30', 'vertex31'},
+    'vertex29': {'vertex25', 'vertex30'},
+    'vertex30': {'vertex28', 'vertex29', 'vertex32'},
+    'vertex31': {'vertex28', 'vertex33'},
+    'vertex32': {'vertex19', 'vertex30'},
+    'vertex33': {'vertex31', 'vertex34', 'vertex35'},
+    'vertex34': {'vertex33', 'vertex37', 'vertex42'},
+    'vertex35': {'vertex33', 'vertex36'},
+    'vertex36': {'vertex35', 'vertex37'},
+    'vertex37': {'vertex34', 'vertex36', 'vertex38'},
+    'vertex38': {'vertex37', 'vertex39'},
+    'vertex39': {'vertex15', 'vertex38', 'vertex40', 'vertex41'},
+    'vertex40': {'vertex17', 'vertex39'},
+    'vertex41': {'vertex39', 'vertex42', 'vertex43'},
+    'vertex42': {'vertex34', 'vertex41'},
+    'vertex43': {'vertex19', 'vertex41'},
+}
+
+
+
 # Bucle principal
 running = True
 while running:
